610229d1265571847b80579a1201ee9463c0df50
Isaac <isaaceteng@gmail.com>
2019-05-15 14:53:51 +0100

    My first DOM commit

---
+++ script.js
@@ -0,0 +1,420 @@
+// document.write (" from Javascript Source <br>");
+
+// for (var i = 1; i <= 12; i++){
+// 	document.write ( "2 x" + i + "=" + 2* i + "<br>");
+// }
+
+// // Assignment 2 Quadratic equation?
+// var a = 6;
+// var b = 11;
+// var c = -35;
+//  // x1 = -b + squrt(b.sq - 4ac)/2a
+//  // x2 = -b - squrt(b.sq - 4ac)/2a
+//  var x1 = ((-b) + Math.sqrt(b*b - 4 * a* c))/(2*a);
+//  var x2 = ((-b) - Math.sqrt(b*b - 4 * a* c))/(2*a);
+//  document.write(" x1 is" + x1 + "<br>");
+//  document.write(" x2 is" + x2 + "<br>");
+// var a = 2;
+// var b = -4;
+// var c = -2;
+// var x1 = ((-b) + Math.sqrt(Math.pow(-b,2) - 4 * a* c))/(2*a);
+// var x2 = ((-b) - Math.sqrt(Math.pow(-b,2) - 4 * a* c))/(2*a);
+// document.write(" x1 is" + ""+ x1 + "<br>");
+// document.write(" x2 is" + ""+ x2 + "<br>");
+
+// // if condition statement
+// var number = 2;
+// var name = "John"
+ 
+//  if (number < 5){
+//  	document.write("Your number is less than 5 <br>" );
+//  }
+
+// if ( name === "John"){
+// 	document.write("Your name is John <br>");
+// }
+
+// var weatherCondition = "raining";
+
+// if (weatherCondition == "raining"){
+// 	document.write(" Take an Umbrella <br>");
+// }
+// else{
+// 	document.write( "Take a Face Cap <br>")
+// }
+
+
+
+// var age = 18;
+
+// if (age < 18){
+// 	document.write("You are not eligible to drink <br>");
+// }
+// else if (age > 18){
+// 	document.write("You are eligible to drink <br>");
+// }
+// else if (age ==18){
+// 	document.write("You are eligible to drink <br>")
+// }
+// else{
+// 	document.write( " Cannot find your age")
+// }
+
+
+// //var age = 10;
+
+// if (age < 18){
+// 	document.write("You are not eligible to drink <br>");
+// }
+// else if (age > 18){
+// 	document.write("You are eligible to drink <br>");
+// }
+// else if (age ==18){
+// 	document.write("You are eligible to drink <br>")
+// }
+// else{
+// 	document.write( " Cannot find your age <br>")
+// }
+
+// //var age = 30;
+
+// if (age < 18){
+// 	document.write("You are not eligible to drink <br>");
+// }
+// else if (age > 18){
+// 	document.write("You are eligible to drink <br>");
+// }
+// else if (age ==18){
+// 	document.write("You are eligible to drink <br>")
+// }
+// else{
+// 	document.write( " Cannot find your age <br>")
+// }
+
+// //var age = "Hello";
+
+// if (age < 18){
+// 	document.write("You are not eligible to drink <br>");
+// }
+// else if (age > 18){
+// 	document.write("You are eligible to drink <br>");
+// }
+// else if (age ==18){
+// 	document.write("You are eligible to drink <br>")
+// }
+// else{
+// 	document.write( " Cannot find your age <br>")
+// }
+
+// //var age = true;
+
+// if (age < 18){
+// 	document.write("You are not eligible to drink <br>");
+// }
+// else if (age > 18){
+// 	document.write("You are eligible to drink <br>");
+// }
+// else if (age ==18){
+// 	document.write("You are eligible to drink <br>")
+// }
+// else{
+// 	document.write( " Cannot find your age <br>")
+// }
+
+
+//functions;
+
+function message1(){
+// 	document.write("Hello, how are you? From message1 <br>");
+// 	document.write("Hello, how are you? From message1 <br>");
+// 	document.write("Hello, how are you? From message1 <br>");
+// 	document.write("Hello, how are you? From message1 <br>");
+// 	document.write("Hello, how are you? From message1 <br>");
+// 	document.write("Hello, how are you? From message1 <br>");
+// 	document.write("Hello, how are you? From message1 <br>");
+// 	document.write("Hello, how are you? From message1 <br>");
+// 	document.write("Hello, how are you? From message1 <br>");
+// 	document.write("Hello, how are you? From message1 <br>");
+// 	document.write("Hello, how are you? From message1 <br>");
+
+// }
+// function message2(){
+// 	document.write("Hello, how are you? From message2 <br>");
+// 	document.write("Hello, how are you? From message2 <br>");
+// 	document.write("Hello, how are you? From message2 <br>");
+// 	document.write("Hello, how are you? From message2 <br>");
+// 	document.write("Hello, how are you? From message2 <br>");
+// 	document.write("Hello, how are you? From message2 <br>");
+// 	document.write("Hello, how are you? From message2 <br>");
+// 	document.write("Hello, how are you? From message2 <br>");
+// 	document.write("Hello, how are you? From message2 <br>");
+// 	//document.write("Hello, how are you? From message2 <br>");
+
+}
+//message1();
+//message2();
+
+//var pie =3.142;
+//var radius = 7;
+//function Area(){
+	//document.write( pie* Math.pow(radius,2) + "<br>");
+//}
+//Area();
+
+
+//function perimeter (){
+//	var l = 15;
+	//document.write( 4*l + "<br>")
+//}
+
+//perimeter();
+//
+//var num1 = 10;
+//var num2 = 2;
+
+//document.write(num1% num2);
+//
+
+// for ( var i = 1; i <=20; i++){
+// 	if ( i % 2 == 0){
+// 		//document.write(i + "<br>");
+// 	}
+// } 
+// for ( var i = 1; i <=20; i++){
+// 	if ( i % 5 == 0){
+// 		//document.write(i + "<br>");
+// 	}
+// } 
+// for ( var i = 1; i <=20; i++){
+// 	if ( i % 10 == 0){
+// 		//document.write(i + "<br>");
+// 	}
+// } 
+
+// // Parameters or Arguments
+// function divide( number1, number2){
+// 	//document.write(number1/number2 + "<br>")
+// }
+// divide(100,10);
+
+// function area(pie, radius){
+// 	//document.write(pie*radius*radius + "<br>");
+// }
+
+// area(3.142,8);
+
+// function bio(name, age, position){
+// 	//document.write(name + " " +age +" "+ position + "<br>");
+// }
+
+// bio("Donald Trump", 74, "American President");
+
+// function bio(name, favoriteFood, favoriteColor){
+// 	//document.write("Name: " + name + " " + "Favorite Food: " + favoriteFood +" "+ "Favorite Color: " + favoriteColor + "<br>");
+// }
+
+// bio("Isaac ", "Eba n Vegetable-Soup ", "Brown");
+// bio("David ", "Fried-rice ", "Red");
+
+// //Callbacks functions:
+
+// function firstName(lastName, age) {
+// 	//document.write("David ");
+// 	lastName();
+// 	age();
+// }
+
+// function lastName() {
+// 	document.write("Eteng ");
+// }
+
+// function age() {
+// 	document.write(74 +"<br>");
+// }
+
+// firstName(lastName, age);
+
+
+// variable scope:
+//var number = 5;
+
+// function num (number ==5){
+// var number = 10; // Local Variable
+// 	document.write(number + "<br>");
+// }
+
+// num();
+// document.write(number + "<br>") // Global variable
+// global variables
+// local variables
+
+// Console
+
+// function banks(){
+// 	var banks = ["First Bank", "Stanbic Bank","GTBank", "Access Bank"];
+// 	for( var i = 0; i<banks.length; i++){
+// 	console.log(banks[i]);
+// 	}
+// }
+
+// banks();
+
+// //Objects
+
+// var student = {
+// 	"name" : "David",
+// 	"exam1":75,
+// 	"exam2":80,
+// 	"exam3":95,
+// 	"programme": "Engineering",
+// 	"greeting" : function(){
+// 		return "Good Afternoon"
+// 		},
+// 	"gender" : function(){
+// 		return "Male";
+// 		},
+// 	};
+
+// //console.log(student.name);
+// ///console.log(student.exam1);
+// //console.log(student.greeting());
+
+// function hospitalNames(){
+// 	var hospitalNames = [ "LUTH", "Lagoon Hospital", "Victoty Hospital", "Grace Clinic", "Ikeja Government House Hospital"];
+// }
+// //console.log(hospitalNames);
+
+// // F: 0 - 39;
+// // D: 40 - 49;
+// // C: 50 - 59;
+// // B: 60 - 69;
+// // A: 70 - 100;
+// // Examination Grading;
+
+// var examScore = -45;
+
+// if (examScore >= 70 && examScore <= 100){
+// //	document.write("You got an A:"+ " "+ examScore);
+// }
+// else if (examScore >= 60 && examScore <= 69){
+// 	//document.write("You got a B:"+ " " + examScore);
+
+// }
+// else if (examScore >= 50 && examScore <= 59){
+// //	document.write("You got a C:"+ " " + examScore);
+// }
+// else if (examScore >= 40 && examScore <= 49){
+// //	document.write("You got a D:"+ " " + examScore);
+// }
+// else if (examScore >= 0 && examScore <= 39){
+// //	document.write("You got a F:"+ " " + examScore);
+// }
+// else{
+// //	document.write("Invalid Exam Score" + "<br>");
+// }
+
+// //  Calling function
+// // Passing variable inside a Function
+// function addition(num1,num2,num3,number4){
+// //	document.write("Add:" + (num1 + num2 + num3 + number4) + "<br>");
+// //	document.write(num1*num3 + "<br>");
+// //	document.write(number4/num2 + "<br>");
+// }
+// addition(3,5,6,10);
+
+// function trapezium(base1,base2,height){
+// //	document.write("Area of the trapezium is "+((base1 + base2)/2)*height + "<br>");
+// }
+// //trapezium(20,26,15);
+
+// // Callbacks
+
+// function breakFast(lunch){
+// //	document.write("Ogi and Akara" +"<br>");
+// 	lunch();
+// }
+// function lunch(){
+// //	document.write("Bread and Butter" + "<br>")
+// }
+
+// //breakFast(lunch);
+
+// // Objects are denoted by{};
+
+// var movieList = {
+// 	'movie1': 'Endgame',
+// 	'movie2': ' Birdbox',
+// 	'movie3': ' Alita',
+// 	'movie4': ' Captain Marvel',
+// 	'movie5': ' Hobbs & Shaw',
+// 	'movie6': ' Jojn Wick',
+// 	'year': 2019,
+// 	'bool': true,
+// 	'interesting': false,
+// 	'rating':null
+// }
+// movieList.movie7 = 'GOT S8';
+// movieList.actor = 'John Doe';
+// //console.log(movieList);
+// //console.log(movieList.movie4);
+
+// // Object inside an Array[{},{},{}]
+//  var countingNumbers = [{'One':1, 'two': 2},{'three':3, 'four': 4},{'five':5, 'six': 6}
+//  ]
+ //console.log(countingNumbers[1].four);
+// console.log(countingNumbers[0].two);
+
+
+//Targetting an ID from html file?
+
+// var lorem = document.getElementById("lorem");
+// //lorem.style.backgroundColor = "green";
+// lorem.style.fontSize = "20px";
+// lorem.style.color = "white";
+// lorem.innerHTML = " Hello, How are you doing?"
+
+// // Targetting a Class from html file?
+// var bio = document.getElementsByClassName("bio");
+// bio[1].style.backgroundColor = "purple";
+// bio[3].style.backgroundColor = "black";
+// bio[1].style.color = "white";
+// bio[3].style.color = "white";
+// bio[0].style.backgroundColor = "red";
+// bio[0].style.color = "white";
+// bio[2].style.backgroundColor = "brown";
+// bio[2].style.color = "white";
+// bio[0].innerHTML = "Name: James Bond";
+// // Using Query  Selector?
+// var loremSelector = document.querySelector("#lorem");
+// loremSelector.style.backgroundColor = "lightgreen";
+
+// var bio = document.querySelectorAll(".bio");
+// bioSelector[0].style.backgroundColor = "blue";
+
+// Creating Alert Message?
+// alert("Good Afternoon");
+// alert("Good day!");
+ 
+// var day = "morning";
+
+// if (day == "morning"){
+// 	alert("Good morning");
+// } 
+// else if (day == "evening"){
+// 	alert ("Good evening");
+// }
+// else{
+// 	alert("Cannot get time of the day");
+// }
+
+// Timing Function
+//function name(){
+//	console.log("James Bond");
+
+//setTimeOut(name, 5000);
+//} 
+
+window.location.href = "index3.html";
+
+
+
